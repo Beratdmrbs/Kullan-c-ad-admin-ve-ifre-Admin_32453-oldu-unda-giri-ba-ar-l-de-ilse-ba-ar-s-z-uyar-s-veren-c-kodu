5fc734bdfa68cdeb3ce07816ca1d3c99234a3673
Beratdmrbs <beratdmrbs61@gmail.com>
2024-11-02 21:08:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string correctUsername = "admin";
+        string correctPassword = "Admin_32453@";
+
+        Console.Write("Kullanıcı adı: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Şifre: ");
+        string password = Console.ReadLine();
+
+        if (username == correctUsername && password == correctPassword)
+        {
+            Console.WriteLine("Giriş başarılı!");
+        }
+        else
+        {
+            Console.WriteLine("Giriş başarısız!");
+        }
+    }
+}
